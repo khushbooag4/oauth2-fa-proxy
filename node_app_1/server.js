ae48638671729c94427486964690af20b32faa3e
Khushboo <kushbooagarwal3584@gmail.com>
2022-07-06 17:01:53 +0530

    node services added

---
+++ node_app_1/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const { auth, requiresAuth } = require("express-openid-connect");
+require("dotenv").config();
+const path = require("path");
+var session = require("express-session");
+const app = express();
+const port = 3001;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(
+  session({
+    secret: "fusionauth",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "40JGJMspPGp2q3VaTGj5pOLbmjHrkGu7QOVIXJHaheg",
+  baseURL: "http://localhost:3001",
+  clientID: "619b9903-0a5a-41f3-9183-6380df21e254",
+  issuerBaseURL: "http://localhost:9011",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    status: req.oidc.isAuthenticated() ? "Logged In" : "Logged Out",
+  });
+});
+
+app.get("/idToken", requiresAuth(), (req, res) => {
+  res.send(
+    "Id Token" +
+      req.oidc.idToken +
+      "Id Claims" +
+      JSON.stringify(req.oidc.idTokenClaims, null, 2)
+  );
+});
+
+app.get("/callback", (req, res) => {
+  console.log(req.oidc);
+});
+
+app.get("/user", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user, null, 2));
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
