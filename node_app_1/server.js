8b9d02c7d8f1ef1998c7913c7c9a810cc51a90df
Khushboo <kushbooagarwal3584@gmail.com>
2022-07-09 18:36:43 +0530

    merged

--- node_app_1/server.js
+++ node_app_1/server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const { auth, requiresAuth } = require("express-openid-connect");
 require("dotenv").config();
 const path = require("path");
+const cookieParser = require("cookie-parser");
 var session = require("express-session");
+var jwt = require("jsonwebtoken");
 const app = express();
 const port = 3001;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 app.use(
   session({
     secret: "fusionauth",
@@ -17,12 +20,13 @@ app.use(
 );
 
 const config = {
-  authRequired: false,
-  auth0Logout: true,
+  authRequired: true,
+  auth0Logout: false,
   secret: "40JGJMspPGp2q3VaTGj5pOLbmjHrkGu7QOVIXJHaheg",
   baseURL: "http://localhost:3001",
-  clientID: "619b9903-0a5a-41f3-9183-6380df21e254",
-  issuerBaseURL: "http://localhost:9011",
+  clientID: "094469e3-23a0-4624-b176-c4d78c9adb2a",
+  issuerBaseURL: "https://fa-op.chakshu-rd.samagra.io/",
+  clientSecret: "40JGJMspPGp2q3VaTGj5pOLbmjHrkGu7QOVIXJHaheg",
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -31,9 +35,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    status: req.oidc.isAuthenticated() ? "Logged In" : "Logged Out",
-  });
+  res.render("auth");
+});
+
+app.get("/signout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.get("/idToken", requiresAuth(), (req, res) => {
@@ -45,12 +52,25 @@ app.get("/idToken", requiresAuth(), (req, res) => {
   );
 });
 
-app.get("/callback", (req, res) => {
-  console.log(req.oidc);
+app.get("/oauth2/callback", (req, res) => {
+  console.log(req.oidc.isAuthenticated());
+  if (req.oidc.isAuthenticated()) {
+    res.render("index", {
+      status: req.oidc.isAuthenticated() ? "User Authenticated" : "User Logout",
+      token: req.oidc.idToken ?? undefined,
+    });
+  } else {
+    res.send("Server Error");
+  }
 });
 
 app.get("/user", requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user, null, 2));
+  // if(req.oidc.isAuthenticated() == false){
+  //   res.redirect('http')
+  // }
+  if (req.oidc.idToken) {
+    res.send(JSON.stringify(req.oidc.user, null, 2));
+  }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
