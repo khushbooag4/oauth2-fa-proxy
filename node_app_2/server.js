3d15823c216bd8625fa73eb0dd1563593ed9df48
Khushboo <kushbooagarwal3584@gmail.com>
2022-07-09 18:36:32 +0530

    merge

--- node_app_2/server.js
+++ node_app_2/server.js
@@ -3,6 +3,7 @@ const { auth, requiresAuth } = require("express-openid-connect");
 const dotenv = require("dotenv").config();
 const path = require("path");
 var session = require("express-session");
+var jwt = require("jsonwebtoken");
 const app = express();
 const port = 3002;
 
@@ -17,12 +18,13 @@ app.use(
 );
 
 const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: "kZ7J1JkxwfzhZHjwaYXtcAlZMS9l1rAjxd7-6CRKaoA",
-  baseURL: "http://localhost:3001",
-  clientID: "619b9903-0a5a-41f3-9183-6380df21e254",
-  issuerBaseURL: "http://localhost:9011",
+  authRequired: true,
+  auth0Logout: false,
+  secret: "40JGJMspPGp2q3VaTGj5pOLbmjHrkGu7QOVIXJHaheg",
+  baseURL: "http://localhost:3002",
+  clientID: "094469e3-23a0-4624-b176-c4d78c9adb2a",
+  issuerBaseURL: "https://fa-op.chakshu-rd.samagra.io/",
+  clientSecret: "40JGJMspPGp2q3VaTGj5pOLbmjHrkGu7QOVIXJHaheg",
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -31,23 +33,19 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    status: req.oidc.isAuthenticated() ? "Looged In" : "Logged Out",
-  });
+  res.send(req.oidc.isAuthenticated() ? "Logged In" : "Logged Out");
 });
 
-app.get("/idToken", requiresAuth(), (req, res) => {
-  res.send(
-    "Id Token" +
-      req.oidc.idToken +
-      "Id Claims" +
-      JSON.stringify(req.oidc.idTokenClaims, null, 2)
-  );
+app.get("/oauth2/callback", (req, res) => {
+  if (req.oidc.isAuthenticated()) {
+    res.render("index", {
+      status: "User Authenticated Successfully",
+    });
+  } else {
+    res.send("Server Error");
+  }
 });
 
-app.get("/callback", (req, res) => {
-  console.log(req.query.err);
-});
 app.get("/user", requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user, null, 2));
 });
